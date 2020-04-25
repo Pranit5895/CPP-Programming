ab60e605f7336a175f4c9b23f88acfc243674912
Pranit5895 <guptapranit9@gmail.com>
2020-04-24 23:35:20 -0700

    Frequency of char in a String

---
+++ Freq-of-char-in-string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str = "C++ Programming is kinda' cool";
+    char checkCharacter = 'a';
+    int count = 0;
+
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] ==  checkCharacter)
+        {
+            ++ count;
+        }
+    }
+
+    cout << "Number of " << checkCharacter << " = " << count;
+
+    return 0;
