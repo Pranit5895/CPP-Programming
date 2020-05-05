470d9c7d8025d59bf1b190074ad1dc1c686a79ea
Pranit5895 <guptapranit9@gmail.com>
2020-05-05 16:58:32 -0700

    main() function with switch case completed

--- Menu-driven.cpp
+++ Menu-driven.cpp
@@ -4,4 +4,27 @@ int binary(int B[],int size,int item);
 void selection(int [],int);
 void bubble(int [],int);
 void insertion(int A[],int size);
+void main()
+{
+cout<<"Enter the size of the array\n";
+int size;
+cin>>size;
+int arr[80];
+for(int i=0;i<size;i++)
+{
+cout<<"Enter the element at position "<<i+1<<" of the array\n";
+cin>>arr[i];
+}
+cout<<"enter your choice,i.e,1 for selection sort and 2 for bubble sort and 3 for insertion sort\n";
+int choice;
+cin>>choice;
+switch(choice)
+{
+case 1:selection(arr,size);
+       break;
+case 2:bubble(arr,size);
+       break;
+case 3:insertion(arr,size);
+       break;
+}
 
