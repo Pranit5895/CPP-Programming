b8fa803601f031a13c2af6f86fe781cefeb3a0a1
Pranit5895 <guptapranit9@gmail.com>
2020-05-05 17:01:26 -0700

    Insertion sort function added

--- Menu-driven.cpp
+++ Menu-driven.cpp
@@ -80,4 +80,22 @@ for(int m=0;m<size;m++)
 cout<<arr1[m]<<" ";
 }
 }
+void insertion(int A[],int size)
+{
+int i,j,key;
+for(i=1;i<size;i++)
+{
+key=A[i];
+j=i-1;
+while(j>=0 && A[j]>key)
+{
+A[j+1]=A[j];
+j=j-1;
+}
+A[j+1]=key;
+}
+cout<<"\nArray after sorting is: ";
+for(int m=0;m<size;m++)
+cout<<A[m]<<" ";
+}
 
