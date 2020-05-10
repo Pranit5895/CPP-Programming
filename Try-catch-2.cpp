62fdee4c4a608fac402e1e7be2824cb9047da4c8
Pranit5895 <guptapranit9@gmail.com>
2020-05-10 03:55:01 -0700

    Input from user

---
+++ Try-catch-2.cpp
@@ -0,0 +1,11 @@
+i #include<iostream.h>
+       #include<conio.h>
+       void main()
+       {
+              int n1,n2,result;
+
+              cout<<"\nEnter 1st number : ";
+              cin>>n1;
+
+              cout<<"\nEnter 2nd number : ";
+              cin>>n2;
