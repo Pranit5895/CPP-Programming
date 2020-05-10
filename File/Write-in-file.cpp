4afacbdb05a51774e7409978bc4e4f66aa2d1d59
Pranit5895 <guptapranit9@gmail.com>
2020-05-10 05:13:51 -0700

    Condition checking and writing in file

--- File/Write-in-file.cpp
+++ File/Write-in-file.cpp
@@ -8,3 +8,16 @@ int main()
 {
     fstream st; // Step 1: Creating object of fstream class
     st.open("E:\studytonight.txt",ios::out);  // Step 2: Creating new file
+    if(!st) // Step 3: Checking whether file exist
+    {
+        cout<<"File creation failed";
+    }
+    else
+    {
+        cout<<"New file created";
+        st<<"Hello";    // Step 4: Writing to file
+        st.close(); // Step 5: Closing file
+    }
+    getch();
+    return 0;
+}
