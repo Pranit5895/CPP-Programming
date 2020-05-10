4ef635228b6be63f97cd54fe39c049f97b9a1984
Pranit5895 <guptapranit9@gmail.com>
2020-05-10 04:01:46 -0700

    main() function completed with template implementation

--- Template.cpp
+++ Template.cpp
@@ -7,3 +7,12 @@ T GetMax (T a, T b) {
   result = (a>b)? a : b;
   return (result);
 }
+int main () {
+  int i=5, j=6, k;
+  long l=10, m=5, n;
+  k=GetMax<int>(i,j);
+  n=GetMax<long>(l,m);
+  cout << k << endl;
+  cout << n << endl;
+  return 0;
+}
