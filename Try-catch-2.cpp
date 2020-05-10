f3123f188a41d3c643a37cea048656dd3939de86
Pranit5895 <guptapranit9@gmail.com>
2020-05-10 03:55:50 -0700

    Try block finished

--- Try-catch-2.cpp
+++ Try-catch-2.cpp
@@ -9,3 +9,13 @@ i #include<iostream.h>
 
               cout<<"\nEnter 2nd number : ";
               cin>>n2;
+	      try
+              {
+                  if(n2==0)
+                      throw n2;         //Statement 1
+                  else
+                  {
+                      result = n1 / n2;
+                      cout<<"\nThe result is : "<<result;
+                  }
+              }
