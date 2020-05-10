555d7833dc7559627afc916d236dffd26c4f3bc0
Pranit5895 <guptapranit9@gmail.com>
2020-05-10 05:27:38 -0700

    function for printing the elements in a list

--- File/List.cpp
+++ File/List.cpp
@@ -1,4 +1,12 @@
 #include <iostream> 
 #include <list> 
 #include <iterator> 
-using namespace std; 
+using namespace std;
+//function for printing the elements in a list
+void showlist(list <int> g)
+{
+    list <int> :: iterator it;
+    for(it = g.begin(); it != g.end(); ++it)
+        cout << '\t' << *it;
+    cout << '\n';
+}
