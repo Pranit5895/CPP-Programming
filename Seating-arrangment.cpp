a5484f2aa53994eaaa06d7a3de7a9359b458d6a4
Pranit5895 <guptapranit9@gmail.com>
2020-05-10 04:07:14 -0700

    Seating arrangment code started

---
+++ Seating-arrangment.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+int f=0,s=9, ans;
+bool firstclass [5] = {0,0,0,0,0};
+bool secondclass [5]= {};
+int fspace = 5;
+int positionint = 0 ;
+int sspace= 8;
+char check, con, position;
+
+using namespace std;
+
+//Function declarations
+void first_class();
+void second_class();
