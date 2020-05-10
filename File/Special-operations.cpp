d458fc705462b6ae7dbbca3e953ebe155af21cc0
Pranit5895 <guptapranit9@gmail.com>
2020-05-10 05:17:55 -0700

    creating a new file for functions of operations

--- File/Special-operations.cpp
+++ File/Special-operations.cpp
@@ -3,4 +3,11 @@
 #include <fstream>
 
 using namespace std;
-
+int main()
+{
+    fstream st; // Creating object of fstream class
+    st.open("E:\studytonight.txt",ios::out);  // Creating new file
+    if(!st) // Checking whether file exist
+    {
+        cout<<"File creation failed";
+    }
