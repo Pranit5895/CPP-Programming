9fda321444031e9aef0761212d9cd891f905db0b
Pranit5895 <guptapranit9@gmail.com>
2020-05-19 04:42:31 -0700

    class Geeks started

---
+++ Class.cpp
@@ -0,0 +1,10 @@
+// C++ program to demonstrate function  
+// declaration outside class 
+  
+#include <bits/stdc++.h> 
+using namespace std; 
+class Geeks 
+{ 
+    public: 
+    string geekname; 
+    int id; 
