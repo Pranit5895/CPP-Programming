65d6a3544ddc748517c75dd4ddb36521aee1301d
Pranit5895 <guptapranit9@gmail.com>
2020-05-20 00:21:56 -0700

    Function to do counting sort of arrays

--- Radix-sort.cpp
+++ Radix-sort.cpp
@@ -10,4 +10,19 @@ int getMax(int arr[], int n)
         if (arr[i] > mx) 
             mx = arr[i]; 
     return mx; 
-} 
+}
+// A function to do counting sort of arr[] according to
+// the digit represented by exp.
+void countSort(int arr[], int n, int exp)
+{
+    int output[n]; // output array
+    int i, count[10] = {0};
+
+    // Store count of occurrences in count[]
+    for (i = 0; i < n; i++)
+        count[ (arr[i]/exp)%10 ]++;
+
+    // Change count[i] so that count[i] now contains actual
+    //  position of this digit in output[]
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
