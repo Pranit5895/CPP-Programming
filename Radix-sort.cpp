dbe95a06004e813af84567d32a44dceebe1db55c
Pranit5895 <guptapranit9@gmail.com>
2020-05-20 00:21:01 -0700

    Fucntion to get max value of array

---
+++ Radix-sort.cpp
@@ -0,0 +1,13 @@
+// C++ implementation of Radix Sort 
+#include<iostream> 
+using namespace std; 
+  
+// A utility function to get maximum value in arr[] 
+int getMax(int arr[], int n) 
+{ 
+    int mx = arr[0]; 
+    for (int i = 1; i < n; i++) 
+        if (arr[i] > mx) 
+            mx = arr[i]; 
+    return mx; 
+} 
