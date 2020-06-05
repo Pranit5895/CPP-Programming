9d46c955a4cb1819b2d01ebda3ad6c47b3788443
Pranit5895 <guptapranit9@gmail.com>
2020-06-05 01:50:18 -0700

    Hackerrank solution for C++

---
+++ Hackerrank/Input-and-Output.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <math.h>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    
+    int a;
+    int b;
+    int c;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+    return 0;
+    
+    
+     
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
