566c4efd0d5417fb2e1d15b70b853e58ac756107
Pranit5895 <guptapranit9@gmail.com>
2020-06-28 01:35:21 -0700

    CPP program to illustrate Destructors

---
+++ Constructor-Destructor/Destructor.cpp
@@ -0,0 +1,21 @@
+class String 
+{ 
+private: 
+    char *s; 
+    int size; 
+public: 
+    String(char *); // constructor 
+    ~String();      // destructor 
+}; 
+  
+String::String(char *c) 
+{ 
+    size = strlen(c); 
+    s = new char[size+1]; 
+    strcpy(s,c); 
+} 
+  
+String::~String() 
+{ 
+    delete []s; 
+} 
